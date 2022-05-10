777e2abf04e0c871430be58d54d05d971fe4a18f
michal-szewo <mszewo@gmail.com>
2022-05-10 13:00:13 +0200

    poprawka w articles.html - wyświetlanie Brak artykułów ...

--- src/main/java/pl/edu/pw/blog/User.java
+++ src/main/java/pl/edu/pw/blog/User.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -38,6 +39,7 @@ public class User implements UserDetails{
 	private Long id;
 	
 	@NonNull
+	@Column(unique = true)
 	private String username;
 	
 	@NonNull
