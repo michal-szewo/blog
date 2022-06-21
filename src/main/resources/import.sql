50202568238a193951400a8b20ab43f819e9e1e3
michal-szewo <mszewo@gmail.com>
2022-06-21 19:45:53 +0200

    poprawka import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 insert into users (id,username,fullname,password) values (1,'m','m','$2a$10$/GmNKy8Mm323RRSk1/mqHOmYUsJZsvJahVxb5XNeAo.Pq5EZchUna');
 insert into users (id,username,fullname,password) values (2,'t','t','$2a$10$/GmNKy8Mm323RRSk1/mqHOmYUsJZsvJahVxb5XNeAo.Pq5EZchUna');
-insert into articles (id,title,body,modified_at,published_at,author_id) values(33333333333,'Lorem ipsum dolor sit amet','consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),2);
+insert into articles (id,title,body,modified_at,published_at,author_id) values(1,'Lorem ipsum dolor sit amet','consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),2);
