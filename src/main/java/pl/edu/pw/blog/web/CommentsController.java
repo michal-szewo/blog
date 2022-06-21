adf6aad62cc7352ea926e7324a2d5820328ab7ab
michal-szewo <mszewo@gmail.com>
2022-06-21 23:16:15 +0200

    kolejne poprawki

--- src/main/java/pl/edu/pw/blog/web/CommentsController.java
+++ src/main/java/pl/edu/pw/blog/web/CommentsController.java
@@ -3,6 +3,11 @@ package pl.edu.pw.blog.web;
 
 
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -11,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import pl.edu.pw.blog.data.Article;
+import pl.edu.pw.blog.data.ArticleRepository;
 import pl.edu.pw.blog.data.Comments;
 
 
@@ -38,24 +45,31 @@ import pl.edu.pw.blog.data.Comments;
 @Controller
 @SessionAttributes("user")
 public class CommentsController{
+	
+	@Autowired
+	private ArticleRepository artRepo;
 
+	Logger log = LoggerFactory.getLogger(ArticlesController.class);
+	
     @PostMapping("/addComment")
     public String addComment(@ModelAttribute("newComment") Comments comment,
                              Model model,
                              RedirectAttributes redirectAttributes
     ) {
 
-        comment.getArticle().getId();
-
-
-        // id_artykulu , id_user
-        // body
-        // article.getComments
-        //add(new Comments(id_artykulu,id_user,body))
-
-//        comment.setAuthor((User) model.getAttribute("user"));
-//        articleRepo.save(article);
-//        redirectAttributes.addFlashAttribute("message","Dodano artykuł o id: "+ article.getId());
+        
+    	Article commentedArticle = comment.getArticle(); 
+    	List<Comments> commentsList = commentedArticle.getComments();
+    	
+    	commentsList.add(comment);
+    	
+    	artRepo.save(commentedArticle);
+    	
+    	model.addAttribute("article",commentedArticle);
+    	
+        log.warn("article: " + comment.getArticle());
+        log.warn("author: " + comment.getUser());
+       
       return "redirect:/";
     }
 
